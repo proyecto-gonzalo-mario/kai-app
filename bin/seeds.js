35227e3041cc97d39e6b024d333b09c2dfe35c0e
Mario Quiroga <marioqui135@gmail.com>
2019-02-18 11:06:24 +0100

    Beaches added and minor changes

--- bin/seeds.js
+++ bin/seeds.js
@@ -4,37 +4,70 @@ const Beach = require ('../models/beach.model');
 
 const playas = [
   {
-    name: "Valdevaqueros",
+    name: "Delta del Ebro",
+    location: {
+      type:'Point',
+      coordinates: [41.12, 0.9367]
+    },
+    sports: ["Kitesurf", "Windsurf"]
+  },
+  {
+    name: "La Manga del Mar Menor",
+    location: {
+      type:'Point',
+      coordinates: [37.6458, -0.7917]
+    },
+    sports: ["Kitesurf", "Windsurf"]
+  },
+  {
+    name: "Roquetas de Mar",
+    location: {
+      type:'Point',
+      coordinates: [36.7469, -2.6188]
+    },
+    sports: ["Kitesurf", "Windsurf"]
+  },
+  {
+    name: "Tarifa",
     location: {
       type:'Point',
       coordinates: [36.0125, -5.6056]
     },
-    sports: ["Kitesurf", "Surf"]
+    sports: ["Kitesurf", "Windsurf", "Surf"]
   },
   {
-    name: "El Postiguet",
+    name: "Somo",
     location: {
       type:'Point',
       coordinates: [43.4506, -3.7358]
     },
-    sports: ["Kitesurf", "Surf"]
+    sports: ["Surf"]
   },
   {
-    name: "El Palmar",
+    name: "Lanzarote",
     location: {
       type:'Point',
-      coordinates: [43.4506, -3.7358]
+      coordinates: [28.05, -15.5833]
     },
-    sports: ["Windsurf", "Surf"]
+    sports: ["Surf"]
   },
   {
-    name: "Altea",
+    name: "Nazare",
     location: {
       type:'Point',
-      coordinates: [36.0125, -5.6056]
+      coordinates: [39.8083, -8.7833]
     },
-    sports: ["Kitesurf", "Surf"]
+    sports: ["Surf"]
   },
+  {
+    name: "Ericeira",
+    location: {
+      type:'Point',
+      coordinates: [39.0367, -9.2515]
+    },
+    sports: ["Surf"]
+  },
+  
 ];
 
 Beach.create(playas)
